05247bbd3199c0f4dc4082cc611fa7acb4d0aa78
A601-11 <tnk.dat.class@gmail.com>
2021-07-01 10:10:01 +0900

    画面作成

--- YoketoruVS21/Form1.Designer.cs
+++ YoketoruVS21/Form1.Designer.cs
@@ -29,13 +29,134 @@ namespace YoketoruVS21
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Title = new System.Windows.Forms.Label();
+            this.StaBut = new System.Windows.Forms.Button();
+            this.Cmaru = new System.Windows.Forms.Label();
+            this.HiSco = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.CL = new System.Windows.Forms.Label();
+            this.GO = new System.Windows.Forms.Label();
+            this.Modoru = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Title.Font = new System.Drawing.Font("プイッコ", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.Title.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Title.Location = new System.Drawing.Point(160, 101);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(433, 72);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "よけとる2021";
+            // 
+            // StaBut
+            // 
+            this.StaBut.BackColor = System.Drawing.Color.Pink;
+            this.StaBut.Font = new System.Drawing.Font("プイッコ", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.StaBut.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.StaBut.Location = new System.Drawing.Point(270, 285);
+            this.StaBut.Name = "StaBut";
+            this.StaBut.Size = new System.Drawing.Size(213, 69);
+            this.StaBut.TabIndex = 1;
+            this.StaBut.Text = "すたーと";
+            this.StaBut.UseVisualStyleBackColor = false;
+            // 
+            // Cmaru
+            // 
+            this.Cmaru.AutoSize = true;
+            this.Cmaru.Location = new System.Drawing.Point(12, 422);
+            this.Cmaru.Name = "Cmaru";
+            this.Cmaru.Size = new System.Drawing.Size(173, 15);
+            this.Cmaru.TabIndex = 2;
+            this.Cmaru.Text = "Copyright Ⓒ 2021 AranKura_Izu";
+            // 
+            // HiSco
+            // 
+            this.HiSco.AutoSize = true;
+            this.HiSco.Font = new System.Drawing.Font("プイッコ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HiSco.Location = new System.Drawing.Point(271, 231);
+            this.HiSco.Name = "HiSco";
+            this.HiSco.Size = new System.Drawing.Size(210, 32);
+            this.HiSco.TabIndex = 4;
+            this.HiSco.Text = "はいすこあ 100";
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("プイッコ", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Time.Location = new System.Drawing.Point(12, 9);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(156, 32);
+            this.Time.TabIndex = 5;
+            this.Time.Text = "たいむ 100";
+            // 
+            // CL
+            // 
+            this.CL.AutoSize = true;
+            this.CL.Font = new System.Drawing.Font("プイッコ", 38.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CL.ForeColor = System.Drawing.Color.Fuchsia;
+            this.CL.Location = new System.Drawing.Point(286, 193);
+            this.CL.Name = "CL";
+            this.CL.Size = new System.Drawing.Size(180, 61);
+            this.CL.TabIndex = 6;
+            this.CL.Text = "くりあ";
+            // 
+            // GO
+            // 
+            this.GO.AutoSize = true;
+            this.GO.Font = new System.Drawing.Font("プイッコ", 38.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GO.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.GO.Location = new System.Drawing.Point(194, 193);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(384, 61);
+            this.GO.TabIndex = 7;
+            this.GO.Text = "げーむおーばー";
+            // 
+            // Modoru
+            // 
+            this.Modoru.BackColor = System.Drawing.Color.Pink;
+            this.Modoru.Font = new System.Drawing.Font("プイッコ", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.Modoru.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.Modoru.Location = new System.Drawing.Point(270, 285);
+            this.Modoru.Name = "Modoru";
+            this.Modoru.Size = new System.Drawing.Size(213, 69);
+            this.Modoru.TabIndex = 8;
+            this.Modoru.Text = "たいとるへ";
+            this.Modoru.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(753, 446);
+            this.Controls.Add(this.CL);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.HiSco);
+            this.Controls.Add(this.Cmaru);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.GO);
+            this.Controls.Add(this.Modoru);
+            this.Controls.Add(this.StaBut);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button StaBut;
+        private System.Windows.Forms.Label Cmaru;
+        private System.Windows.Forms.Label HiSco;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Label CL;
+        private System.Windows.Forms.Label GO;
+        private System.Windows.Forms.Button Modoru;
     }
 }
 
